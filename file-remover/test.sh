5672ece450d7b3198eee2aa6da49aad25590d78d
Paul Shevchuk <iVisualPaul@gmail.com>
2015-09-12 10:54:56 +0300

    test script now create its directories in /tmp/

--- file-remover/test.sh
+++ file-remover/test.sh
@@ -1,20 +1,20 @@
 #/usr/bin/env bash
 #if encoding is different from UTF-8 it may not work... Nevermind
-
+root=`pwd`
 for i in a b c d; do
-    mkdir $i
-    cd $i
-    xargs -d '\n' -a ../$i.files touch
-    cd ..
-    ./file-remover.sh $i < $i.test
-    if [ ! -e $i ]; then
+    mkdir /tmp/$i
+    pushd /tmp/$i > /dev/null
+    xargs -d '\n' -a $root/$i.files touch
+    popd > /dev/null
+    ./file-remover.sh /tmp/$i < $i.test
+    if [ ! -e /tmp/$i ]; then
         echo -e "Test $i: \e[31mFAILED\e[39m"
         continue
     fi
-    if ls $i | diff -b - $i.result; then
+    if ls /tmp/$i | diff -b - $i.result; then
         echo -e "Test $i: \e[32mPASSED\e[39m"
     else
         echo -e "Test $i: \e[31mFAILED\e[39m"
     fi
-    rm -r $i
+    rm -r /tmp/$i
 done
